73abfa958fbb0b1df8cd273f1ee8dfa37c161bd2
Jackmena <mena.jack72@gmail.com>
2022-10-03 20:02:58 -0300

    add lesson 2-11

---
+++ 2_FUNDAMENTOS/11_event_loop/index.js
@@ -0,0 +1,14 @@
+function a() {
+  console.log("Execeutando a()");
+}
+
+function b() {
+  console.log("Execeutando b()");
+}
+function c() {
+  console.log("Execeutando c()");
+}
+
+b();
+c();
+a();
