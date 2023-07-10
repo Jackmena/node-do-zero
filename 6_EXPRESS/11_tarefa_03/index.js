023d43274904cf7af16520086cf079f8d6969949
Jackson Mena <mena.jack72@gmail.com>
2023-07-10 14:49:02 -0300

    create css

--- 6_EXPRESS/11_tarefa_03/index.js
+++ 6_EXPRESS/11_tarefa_03/index.js
@@ -5,6 +5,8 @@ const path = require('path')
 
 const basePath = path.join(__dirname, 'templates')
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
     res.sendFile(`${basePath}/index.html`)
 })
