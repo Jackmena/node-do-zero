b7a0f192aeec533322b912f1b10df3a8f2974f15
Jackson Mena <mena.jack72@gmail.com>
2023-07-10 15:34:54 -0300

    add router task 03

--- 6_EXPRESS/11_tarefa_03/index.js
+++ 6_EXPRESS/11_tarefa_03/index.js
@@ -3,9 +3,13 @@ const app = express()
 const port = 5000
 const path = require('path')
 
+const pagesRouter = require('./router')
+
 const basePath = path.join(__dirname, 'templates')
 
 app.use(express.static('public'))
+
+app.use('/', pagesRouter)
 
 app.get('/', (req, res) => {
     res.sendFile(`${basePath}/index.html`)
