df97e6935c0055859e117d7908c0078f0a90a298
Jackson Mena <mena.jack72@gmail.com>
2023-07-12 10:59:16 -0300

    add lesson 7-09

---
+++ 7_TEMPLATE_ENGINE/9_css/index.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+const hbs = exphbs.create({
+    partialsDir: ['views/partials']
+})
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/dashboard', (req, res) => {
+    const items = ['Item a', 'Item b', 'Item c']
+
+    res.render('dashboard', { items })
+})
+
+app.get('/post', (req, res) => {
+
+    const post = {
+        title: 'Aprender Node.js',
+        category: 'JavaScript',
+        body: 'Este artigo vai te ajudar a aprender Nde.js...',
+        comments: 4,
+    }
+
+    res.render('blogpost', { post })
+})
+
+app.get('/blog', (req, res) => {
+    const posts = [
+        {
+            title: 'Aprender Node.js',
+            category: 'JavaScript',
+            body: 'Teste',
+            comments: 3,
+        },
+        {
+            title: 'Aprender PHP',
+            category: 'PHP',
+            body: 'Teste',
+            comments: 3,
+        },
+        {
+            title: 'Aprender Python',
+            category: 'Python',
+            body: 'Teste',
+            comments: 3,
+        }
+    ]
+    res.render('blog', { posts })
+})
+
+app.get('/', (req, res) => {
+    const user = {
+        name: 'Jackson',
+        surname: 'Mena',
+        age: 31,
+    }
+
+    const auth = false
+
+    const approved = false
+
+    res.render('home', { user: user, auth, approved })
+})
+
+app.listen(3000, () => {
+    console.log('App funcionando!')
+})
