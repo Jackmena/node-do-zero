ec6e3d6d2fab9b2d8a09c47bc108402ed5f12aef
Jackson Mena <mena.jack72@gmail.com>
2023-07-12 15:13:26 -0300

    update product and itens task 04

--- 7_TEMPLATE_ENGINE/10_tarefa_04/router/index.js
+++ 7_TEMPLATE_ENGINE/10_tarefa_04/router/index.js
@@ -1,27 +1,36 @@
 const express = require('express')
 const router = express.Router()
 
+const products = [
+    {
+        title: 'Xbox One',
+        price: 'R$ 1.500,00',
+        category: 'Games',
+        description: 'Console da Microsoft',
+        id: 1,
+    },
+    {
+        title: 'PS5',
+        price: 'R$ 3.999,00',
+        category: 'Games',
+        description: 'Console da Sony',
+        id: 2,
+    },
+    {
+        title: 'Nintendo Switch',
+        price: 'R$ 2.300,00',
+        category: 'Games',
+        description: 'Console da Nintendo',
+        id: 3,
+    },
+]
+
+router.get('/itens/:id', (req, res) => {
+    const item = products[parseInt(req.params.id) - 1]
+    res.render('itens', { item })
+})
+
 router.get('/', (req, res) => {
-    const products = [
-        {
-            title: 'one',
-            price: 'R$ 1,00',
-            category: 'info',
-            description: 'Alta qualidade',
-        },
-        {
-            title: 'two',
-            price: 'R$ 2,00',
-            category: 'info',
-            description: 'Alta qualidade',
-        },
-        {
-            title: 'three',
-            price: 'R$ 3,00',
-            category: 'info',
-            description: 'Alta qualidade',
-        },
-    ]
     res.render('home', { products })
 })
 
